07106c06a00546f7f86b3f116cd47ab1124beeb4
102213046 <s102213046@mail1.ncnu.edu.tw>
2015-12-07 21:29:42 +0800

    新增檔案 主畫面框架

---
+++ main.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<style>
+p{
+	font-size:20px;
+	line-height: 30px;
+}
+</style>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>快樂廚房</title>
+<div style="position: absolute; border: 1px solid; width: 1000px; height: 550px ;right: 10px;">
+	
+</div>
+<p>玩家名稱：<br/>
+經驗值：<br/>
+等級：<br/>
+金幣：</p><br/>
+<button style="position:absolute; bottom:10px; left:10px; width:120px; height:40px; font-size:20px">材料包</button>
+<button style="position:absolute; bottom:10px; left:140px;width:120px; height:40px; font-size:20px;">麵包圖鑑</button>
+<button style="position:absolute; bottom:10px; right:10px;width:120px; height:40px; font-size:20px;">返回</button>
+</head>
+<body>
+
+
+
+</body>
+</html>
