fa4109dce77d6d5a3693d2d3a230c02afefdbf09
hisean102213016 <s102213016@mail1.ncnu.edu.tw>
2015-12-23 19:12:37 +0800

    Update bread.php

--- bread.php
+++ bread.php
@@ -1,8 +1,13 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>快樂廚房</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- 最新編譯和最佳化的 CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+<!-- 選擇性佈景主題 -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="css/main.css">
 <style type="text/css">
 body {
 background-color:#DAA520;
@@ -10,116 +15,60 @@ background-image:url(back2.jpg);
 background-opacity:0.1;
 background-position: 150px 300px;
 }
-#container {
-/* box-sizing: border-box; */
-width: 900px;
-margin: 20px auto;
-padding:20px;
-border: 1px solid #ccc;
-position:relative;
-background-color:#efc;
-}
-#banner {
-color:ivory;
-background-color:darkOliveGreen;
-padding:20px;
-}
-#header {
-position:absolute;
-padding:2px;
-left:30px;
-font-size:14pt;
-}
-
-#user {
-position:absolute;
-top:70px;
-background: linear-gradient(to right,#CD853F 60%,#FFDAB9);
-padding:5px 0px 3px 10px;
-border-left:5px double #800000;
-border-bottom: 1px solid black;
-font-size: 14pt;
-letter-spacing: 4pt;
-opacity:0.7;
-}
-h1{
-font-size:17pt;
-top:40px;
-text-indent: 1pc;
-color:#DAA520;
-font-family:Bradley Hand ITC;
-text-align:center;
-background-image:url(banner4.jpg);
-background-repeat: no-repeat;
-background-position: center;
-opacity:0.5;
-}
-
-#table {
-width: 725px;
-position:absolute;
-left:330px;
-top:70px;
-font-size: 14pt;
-td {text-align: left;}
-td+td {text-align:left; padding-left:10px}
-
-
-}
-#picture {
-position:absolute;
-left:1150px;
-top:350px;
-opacity:0.35;
-}
-#picture2 {
-position:absolute;
-left:30px;
-top:350px;
-opacity:0.35;
-}
-
-
 </style>
+</style>
+<title>快樂廚房</title>
+</head>
 <body>
-<div id="header">
-<p>開心廚房 </p>
-</div>
-<div id="user">
-<p>玩家名稱:</p>
-<p>經驗值:</p>
-<p>等級:</p>
-<p>金幣:</p>
-</div>
-<h1>麵包圖鑑</h1>
-<div id="table">
-<table align="center"  cellspacing="10" cellpadding="8">
-<thead><tr>
-<th>紅豆麵包<img src="images.jpg"width="80"/><br/>價錢:20元<br/>時間:5min<br/>庫存:10個<br/><button>+</button><button>-</button><br/>數量:</th>
-<th>乳酪麵包<img src="images.jpg"width="80"/><br/>價錢:20元<br/>時間:5min<br/>庫存:10個<br/><button>+</button><button>-</button><br/>數量:</th>
-<th>蔥花麵包<img src="images.jpg"width="80"/><br/>價錢:20元<br/>時間:5min<br/>庫存:10個<br/><button>+</button><button>-</button><br/>數量:</th>
-<th>炸彈麵包<img src="images.jpg"width="80"/><br/>價錢:20元<br/>時間:5min<br/>庫存:10個<br/><button>+</button><button>-</button><br/>數量:</th>
-</tr></thead>
-<thead><tr>
-<th>法國麵包<img src="images.jpg"width="80"/><br/>價錢:20元<br/>時間:5min<br/>庫存:10個<br/><button>+</button><button>-</button><br/>數量:</th>
-<th>牛奶土司<img src="images.jpg"width="80"/><br/>價錢:20元<br/>時間:5min<br/>庫存:10個<br/><button>+</button><button>-</button><br/>數量:</th>
-<th>葡萄吐司<img src="images.jpg"width="80"/><br/>價錢:20元<br/>時間:5min<br/>庫存:10個<br/><button>+</button><button>-</button><br/>數量:</th>
-<th>燕麥吐司<img src="images.jpg"width="80"/><br/>價錢:20元<br/>時間:5min<br/>庫存:10個<br/><button>+</button><button>-</button><br/>數量:</th>
-</tr></thead>
-<thead><tr><th>總數:</th><th>交易金額:</th><th></th><th><button>交易</button></th></tr></thead>
-</table>
-</div>
-<div id="bottom">
-<button style="position:absolute; bottom:10px; left:10px; width:120px; height:40px; font-size:20px">材料包</button>
-<button style="position:absolute; bottom:10px; left:140px;width:120px; height:40px; font-size:20px;">烤箱</button>
-<button onclick="" style="position:absolute; bottom:10px; right:10px;width:120px; height:40px; font-size:20px;">返回</button>
-</div>
-<div id="picture">
-<p><img src="chef.jpg"width="200"/ ></p>
-</div>
-<div id="picture2">
-<p><img src="chef2.jpg"width="250"/ ></p>
-</div>
+<div class="container-fluid">
+    <div class="row" id="wrapper">
+        <div class="col-md-12" id="section1">
+            <div class="row no-gutter" id="section1-1">
+                <div class="col-md-3" id="leftbar">
+                    <p>玩家名稱：<br/>
+                    經驗值：<br/>
+                    等級：<br/>
+                    金幣：</p><br/>
+                </div>
+                <div class="col-md-9" id="table">
+                   <table align="center"  cellspacing="10" cellpadding="8">
+				   <thead><tr>
+				   <th>紅豆麵包<br/><img src="1.jpg"width="200"height="150"/><br/>價錢:25元<br/>時間:5min<br/>庫存:10個<br/><button>+</button><button>-</button><br/>數量:</th>
+				   <th>乳酪麵包<br/><img src="2.jpg"width="200"height="150"/><br/>價錢:35元<br/>時間:5min<br/>庫存:10個<br/><button>+</button><button>-</button><br/>數量:</th>
+				   <th>蔥花麵包<br/><img src="3.jpg"width="200"height="150"/><br/>價錢:20元<br/>時間:5min<br/>庫存:10個<br/><button>+</button><button>-</button><br/>數量:</th>
+				   <th>炸彈麵包<br/><img src="4.jpg"width="200"height="150"/><br/>價錢:35元<br/>時間:5min<br/>庫存:10個<br/><button>+</button><button>-</button><br/>數量:</th>
+				   </tr></thead>
+				   <thead><tr>
+				   <th>法國麵包<br/><img src="5.jpg"width="200"height="150"/><br/>價錢:40元<br/>時間:5min<br/>庫存:10個<br/><button>+</button><button>-</button><br/>數量:</th>
+				   <th>牛奶土司<br/><img src="6.jpg"width="200"height="150"/><br/>價錢:60元<br/>時間:5min<br/>庫存:10個<br/><button>+</button><button>-</button><br/>數量:</th>
+				   <th>葡萄吐司<br/><img src="7.jpg"width="200"height="150"/><br/>價錢:65元<br/>時間:5min<br/>庫存:10個<br/><button>+</button><button>-</button><br/>數量:</th>
+				   <th>燕麥吐司<br/><img src="8.jpg"width="200"height="150"/><br/>價錢:80元<br/>時間:5min<br/>庫存:10個<br/><button>+</button><button>-</button><br/>數量:</th>
+				   </tr></thead>
 
+				   <thead><tr><th>交易金額:</th><th></th><th><button>交易</button></th></tr></thead>
+
+				   </table>
+                </div>        
+            </div>
+            <div class="row no-gutter">
+                <div class="col-md-12" id="space"> </div>
+            </div>
+            <div class="row no-gutter" id="button">
+                <div class="col-md-1 col-xs-4">
+                    <button>材料包</button>
+                </div>
+                <div class="col-md-1 col-xs-4">
+                    <button>烤箱</button>
+                </div>
+                <div class="col-md-1 col-xs-4 col-md-offset-9">
+                    <button>返回</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+<!-- 最新編譯和最佳化的 JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
