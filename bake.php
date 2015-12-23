7e73831115da910779769aa9b1d3768bf156e2d3
june52tw <s102213050@mail1.ncnu.edu.tw>
2015-12-24 00:58:23 +0800

    bake only position

--- bake.php
+++ bake.php
@@ -1,40 +1,64 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-		<link rel="stylesheet" href="css/bake.css">
-<style>
-p{
-	font-size:20px;
-	line-height: 30px;
-}
-</style>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- 最新編譯和最佳化的 CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+<!-- 選擇性佈景主題 -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="css/bake.css">
 <title>快樂廚房</title>
-<div class="container">
-	<div class="inside">
-        <div class="inside-img">
-            <button onclick="self.location.href='bake2.php'" class="oven" id="oven" style="background-image:url(img/oven.png);"></button>
-            <button onclick="self.location.href='bake2.php'" class="oven" id="oven" style="background-image:url(img/oven.png);"></button>
-            <button onclick="self.location.href='bake2.php'" class="oven" id="oven" style="background-image:url(img/oven.png);"></button></br>
-            <button onclick="self.location.href='bake2.php'" class="oven" id="oven" style="background-image:url(img/oven.png);"></button>
-            <button onclick="self.location.href='bake2.php'" class="oven" id="oven" style="background-image:url(img/oven.png);"></button>
-            <button onclick="self.location.href='bake2.php'" class="oven" id="oven" style="background-image:url(img/oven.png);"></button>
+</head>
+<body>
+<div class="container-fluid">
+    <div class="row" id="wrapper">
+        <div class="col-md-12" id="section1">
+            <div class="row no-gutter" id="section1-1">
+                <div class="col-md-3" id="leftbar">
+                    <p>玩家名稱：<br/>
+                    經驗值：<br/>
+                    等級：<br/>
+                    金幣：</p><br/>
+                </div>
+                <div class="col-md-9" id="main">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-6" id="oven1">
+                                <button type="button" class="oven btn btn-link" onclick=""></button>
+                            </div>
+                            <div class="col-md-6" id="oven2">
+                                <button type="button" class="oven btn btn-link" onclick=""></button>
+                            </div>
+                            <div class="col-md-6" id="oven3">
+                                <button type="button" class="oven btn btn-link" onclick=""></button>
+                            </div>
+                            <div class="col-md-6" id="oven4">
+                                <button type="button" class="oven btn btn-link" onclick=""></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>        
+            </div>
+            <div class="row no-gutter">
+                <div class="col-md-12" id="space"> </div>
+            </div>
+            <div class="row no-gutter" id="button">
+                <div class="col-md-1 col-xs-4">
+                    <button>材料包</button>
+                </div>
+                <div class="col-md-1 col-xs-4">
+                    <button>麵包圖鑑</button>
+                </div>
+                <div class="col-md-1 col-xs-4 col-md-offset-9">
+                    <button>返回</button>
+                </div>
+            </div>
         </div>
     </div>
 </div>
-<p>玩家名稱：<br/>
-經驗值：<br/>
-等級：<br/>
-金幣：</p><br/>
-<button style="position:absolute; bottom:10px; left:10px; width:120px; height:40px; font-size:20px">材料包</button>
-<button style="position:absolute; bottom:10px; left:140px;width:120px; height:40px; font-size:20px;">麵包圖鑑</button>
-<button onclick="self.location.href='kitchen.php'" style="position:absolute; bottom:10px; right:10px;width:120px; height:40px; font-size:20px;">返回</button>
-</head>
-<body>
-
-
-
+<script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+<!-- 最新編譯和最佳化的 JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
