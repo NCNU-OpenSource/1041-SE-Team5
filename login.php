8de9882edf07cb9ba4dcb365d6a23ffab9ef7c44
102213046 <s102213046@mail1.ncnu.edu.tw>
2015-12-25 00:11:31 +0800

    login背景新增

--- login.php
+++ login.php
@@ -12,28 +12,14 @@
 <style type="text/css">
 body {
 background-color:#DAA520;
-background-image:url(back2.jpg);
+background-image:url(login1.png);
+background-size:100% 100%;
 background-opacity:0.1;
-background-position: 150px 300px;
+background-repeat: no-repeat;
+background-size:cover;
 }
-.div_table-cell{
-	width:450px; 
-	height:240px;	
 
-	display:table-cell; 
-	text-align:center; 
-	vertical-align:middle;
-	border:solid 2px #fff;	
-	}
- 
-/* IE6 hack */
-.div_table-cell span{
-	height:100%; 
 
-	}
- 
-/* 讓table-cell下的所有元素都居中 */
-.div_table-cell *{ vertical-align:middle;}
 </style>
 <?php
 session_start();
@@ -78,20 +64,13 @@ if(isset( $_POST['id'])){
 		
 ?>
 <body>
-<div style="text-align:center">
-<div id="picture">
-<img src="menu3.jpg" >
-</div>
-<h1>開心廚房</h1><hr />
 
-<div class="div_table-cell">
+<div style="position: absolute;bottom: 0;left:100px">
 <form method="post" action="regist.php">
 User Name: <input type="text" name="id"><br />
 Password : <input type="password" name="pwd"><br />
 <input type="image" value="Log in" src="regist.jpg" >
 </form>
-</div>
-<div class="div_table-cell">
 <form method="post" action="login.php">
 User Name: <input type="text" name="id"><br />
 Password : <input type="password" name="pwd"><br />
@@ -99,7 +78,6 @@ Password : <input type="password" name="pwd"><br />
 </form>
 </div>
 
-</div>
 
 <script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
 <!-- 最新編譯和最佳化的 JavaScript -->
