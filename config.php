35184d84acd0f1d89720e9b72fc00b13c8920acf
102213046 <s102213046@mail1.ncnu.edu.tw>
2015-12-27 14:00:17 +0800

    index增加+登入檢查

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+//將所有頁面會用到的都放在這裡，需要使用再用require("congig.php")呼叫,but login.php可能會有問題
+session_start();
+$host = 'localhost'; 
+$user = 'Gina';
+$pass = '48477984';
+$db = 'kitchen';
+$conn = mysqli_connect($host, $user, $pass,$db) or die('Error with MySQL connection'); //跟MyMSQL連線並登入
+mysqli_query($conn,"SET NAMES utf8"); //選擇編碼
+//mysql_select_db($db, $conn); //選擇資料庫
+
+if(!isset( $_SESSION["uID"]))
+	header("Location:login.php");
+	
+if($_SESSION["uID"]=="")
+	header("Location:login.php");
+
+?>
