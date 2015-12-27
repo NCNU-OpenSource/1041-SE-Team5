b3eb14da816255f1c11073fb23cbd87268f46188
june52tw <s102213050@mail1.ncnu.edu.tw>
2015-12-27 23:02:51 +0800

    start baking

---
+++ bakestart.php
@@ -0,0 +1,27 @@
+<?php
+include"config.php";
+$id=$_SESSION['pname'];
+   /*GET¶Ç¨Óªº­È*/
+$material = $_POST["m_num"];
+$oven = $_POST["oven_s"];
+
+$sql1 = "select * from player where pname='$id'";
+$result1=mysqli_query($conn,$sql1);
+$rs1=mysqli_fetch_array($result1);
+
+$sql = "select * from bread where m_num='$material' and avallevel<='".$rs1['level']."'";
+if($results=mysqli_query($conn,$sql)){
+    if($rs=mysqli_fetch_array($results)){
+        $time=date('U');
+        $sqlb = "update ovenplayer set bid=".$rs[bid]." , btime='".$time."'+'".$rs['baketime']."', status=1 where pname='".$id."' and oid='".$oven."'";
+        mysqli_query($conn,$sqlb)or die("MySQL query error");
+        $sqlc = "update player set material=material-'".$material."' where pname='".$id."'";
+        mysqli_query($conn,$sqlc)or die("MySQL query error");
+        echo 'success';
+        header("Location:bake.php");
+    }
+} else {
+    echo 'Error!!';
+}
+
+?>
