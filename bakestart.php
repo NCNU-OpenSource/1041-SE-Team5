4cceecb48df0f78a532a3c1c2e1e7bf0e48f418e
june52tw <s102213050@mail1.ncnu.edu.tw>
2016-01-03 21:29:52 +0800

    small fix

--- bakestart.php
+++ bakestart.php
@@ -12,7 +12,7 @@ $rs1=mysqli_fetch_array($result1);
 $sql = "select * from bread where m_num='$material' and avallevel<='".$rs1['level']."'";
 if($results=mysqli_query($conn,$sql)){
     if($rs=mysqli_fetch_array($results)){
-        $time=date('U');
+        $time=date('U')+25200;
         $sqlb = "update ovenplayer set bid=".$rs[bid]." , btime='".$time."'+'".$rs['baketime']."', status=1 where pname='".$id."' and oid='".$oven."'";
         mysqli_query($conn,$sqlb)or die("MySQL query error");
         $sqlc = "update player set material=material-'".$material."' where pname='".$id."'";
