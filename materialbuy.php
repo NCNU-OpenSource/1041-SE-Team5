77d54d7803a8f44b47431055e7699265a6d10277
102213036 <s102213036@mail1.ncnu.edu.tw>
2016-01-07 17:14:10 +0800

    material.php materialbuy.php

--- materialbuy.php
+++ materialbuy.php
@@ -1,63 +1,63 @@
 <?php
 include"config.php";
-$pname=$_SESSION['pname'];
+$pid=$_SESSION['uID'];
    /*GET傳來的值*/
-$sql = "select * from playerbread where pname=$pname;";
+$sql = "select * from player where pid='$pid';";
 
 $results=mysqli_query($conn,$sql);
-$sqlcoin = "select * from player where pname=$pname;";
+$sqlcoin = "select * from player where pid='$pid';";
 
 $results=mysqli_query($conn,$sqlcoin);
 $id=(int)$_GET["id"];
 
 
     if($id==1){
-        $sql1 = "update player set material=material+1  where pname='$pname' ";
+        $sql1 = "update player set material=material+1  where pid='$pid' ";
         mysqli_query($conn,$sql1) or die("MySQL query error"); 
-        $sqlc = "update player set coin=coin-15  where pname='$pname' ";
+        $sqlc = "update player set coin=coin-15  where pid='$pid' ";
         mysqli_query($conn,$sqlc) or die("MySQL query error"); 		
     }
 						   
     if($id==2){
-        $sql2 = "update player set material=material+1 where pname='$pname'  ";
+        $sql2 = "update player set material=material+1 where pid='$pid'  ";
         mysqli_query($conn,$sql2) or die("MySQL query error");
-        $sqlc = "update player set coin=coin-15  where pname='$pname' ";
+        $sqlc = "update player set coin=coin-15  where pid='$pid' ";
         mysqli_query($conn,$sqlc) or die("MySQL query error"); 		
     }
     if($id==3){
-        $sql3 = "update player set material=material+2 where pname='$pname'  ";
+        $sql3 = "update player set material=material+2 where pid='$pid'  ";
         mysqli_query($conn,$sql3) or die("MySQL query error");
-        $sqlc = "update player set coin=coin-30  where pname='$pname' ";
+        $sqlc = "update player set coin=coin-30  where pid='$pid' ";
         mysqli_query($conn,$sqlc) or die("MySQL query error"); 		
     }
     if($id==4){
-        $sql4 = "update player set material=material+2 where pname='$pname' ";
+        $sql4 = "update player set material=material+2 where pid='$pid' ";
         mysqli_query($conn,$sql4) or die("MySQL query error");
-        $sqlc = "update player set coin=coin-30  where pname='$pname' ";
+        $sqlc = "update player set coin=coin-30  where pid='$pid' ";
         mysqli_query($conn,$sqlc) or die("MySQL query error"); 		
     }
     if($id==5){
-        $sql5 = "update player set material=material+2 where pname='$pname' ";
+        $sql5 = "update player set material=material+2 where pid='$pid' ";
         mysqli_query($conn,$sql5) or die("MySQL query error");
-        $sqlc = "update player set coin=coin-30  where pname='$pname' ";
+        $sqlc = "update player set coin=coin-30  where pid='$pid' ";
         mysqli_query($conn,$sqlc) or die("MySQL query error"); 		
     }
     if($id==6){
-        $sql6 = "update player set material=material+3 where pname='$pname'  ";
+        $sql6 = "update player set material=material+3 where pid='$pid'  ";
         mysqli_query($conn,$sql6) or die("MySQL query error");
-        $sqlc = "update player set coin=coin-45  where pname='$pname' ";
+        $sqlc = "update player set coin=coin-45  where pid='$pid' ";
         mysqli_query($conn,$sqlc) or die("MySQL query error"); 		
     }
     if($id==7){
-        $sql7 = "update player set material=material+3 where pname='$pname'  ";
+        $sql7 = "update player set material=material+3 where pid='$pid'  ";
         mysqli_query($conn,$sql7) or die("MySQL query error");
-        $sqlc = "update player set coin=coin-45  where pname='$pname' ";
+        $sqlc = "update player set coin=coin-45  where pid='$pid' ";
         mysqli_query($conn,$sqlc) or die("MySQL query error"); 		
     }
     if($id==8){
-        $sql8 = "update player set material=material+3 where pname='$pname'  ";
+        $sql8 = "update player set material=material+3 where pid='$pid'  ";
         mysqli_query($conn,$sql8) or die("MySQL query error");
-        $sqlc = "update player set coin=coin-45  where pname='$pname' ";
+        $sqlc = "update player set coin=coin-45  where pid='$pid' ";
         mysqli_query($conn,$sqlc) or die("MySQL query error"); 		
     }
 
